e7f14dd665a9f144c50de39932bd08ca824976e1
donkz <donkz@users.noreply.github.com>
2020-11-29 23:55:27 -0600

    more broken rtcwpro stats

--- rtcwlog/constants/logtext.py
+++ rtcwlog/constants/logtext.py
@@ -194,7 +194,7 @@ class Const:
             ["prosepar", "^--------------------------------------------------------------------------",EVENT_PRO_SEPARATOR, "", False],
             
             ["roundind", "^Accuracy info for: (.*)", EVENT_OSP_STATS_ACCURACY, "", False],
-            ["roundindpro", "^Overall stats for: (.*)", EVENT_OSP_STATS_ACCURACY, "", False],
+            ["roundindpro", "Overall stats for: (.*)", EVENT_OSP_STATS_ACCURACY, "", False],
             ["timelimitmain", "^\[skipnotify\]Timelimit hit\.",EVENT_MAIN_TIMELIMIT, "", False],
             ["clocksetmain", "^\[skipnotify\]Server: timelimit changed to (.*)",EVENT_MAIN_SERVER_TIME, "", False],
             #match ending osp
